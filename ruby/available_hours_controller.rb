fa43b90de3a25622534a55b8d4e45b53a2ab42db
Diego Aleman <diegoalemtz@gmail.com>
2019-02-21 15:54:40 -0600

    Refactor of available schedule functionality at index method

--- ruby/available_hours_controller.rb
+++ ruby/available_hours_controller.rb
@@ -1,38 +1,8 @@
 class AvailableHoursController < ApiController
   def index
-    consultation_duration = account.configuration['schedule']['consultation_duration']
-    schedules = account_schedules(consultation_duration.minutes)
-
-    consultation_schedules.each do |schedule|
-      day = schedule.starts_at.strftime('%a').downcase
-      next if schedules[day].empty?
-      rounded_times = CalendarTimeCalculation
-                      .round_times(account, schedule, consultation_duration, day)
-
-      current_parsed_time = CalendarTimeCalculation.hour_parser(rounded_times.first)
-      end_schedule_parsed = CalendarTimeCalculation.hour_parser(rounded_times.last)
-
-      # Travel and block each slot from current to end
-      until current_parsed_time >= end_schedule_parsed
-        schedule_time_parsed = CalendarTimeCalculation.minute_parser(current_parsed_time)
-
-        index = schedules[day].index(schedule_time_parsed)
-        schedules[day].delete_at(index) if index # Make sure the slot exists
-
-        current_parsed_time += consultation_duration
-      end
-
-      last_val = schedules[day].index(rounded_times.last)
-      # If Schedule ends after schedule ends parsed, delete the last slot
-      if CalendarTimeCalculation.hour_parser(schedule.ends_at.strftime('%H:%M')) >
-         end_schedule_parsed && last_val
-        schedules[day].delete_at(last_val)
-      end
-    end
-
-    render json: { hours: schedules }
+		render json: { hours: available_schedules }
   end
-
+	
   private
   def week
     if params[:from] && params[:to]
@@ -49,6 +19,48 @@ class AvailableHoursController < ApiController
   def consultation_schedules
     ConsultationSchedule.where(starts_at: week, account: account)
   end
+
+	def hour_parser(time)
+		CalendarTimeCalculation.hour_parser(time)
+	end
+
+	def minute_parser(time)
+		CalendarTimeCalculation.minute_parser(time)
+	end
+
+	def available_schedules
+		consultation_duration = account.configuration['schedule']['consultation_duration']
+		schedules = account_schedules(consultation_duration.minutes)
+
+		consultation_schedules.each do |schedule|
+			day = schedule.starts_at.strftime('%a').downcase
+			next if schedules[day].empty?
+			rounded_times = CalendarTimeCalculation
+				.round_times(account, schedule, consultation_duration, day)
+
+			current_parsed_time = hour_parser(rounded_times.first)
+			end_schedule_parsed = hour_parser(rounded_times.last)
+
+			# Travel and block each slot from current to end
+			until current_parsed_time >= end_schedule_parsed
+				schedule_time_parsed = minute_parser(current_parsed_time)
+
+				index = schedules[day].index(schedule_time_parsed)
+				schedules[day].delete_at(index) if index # Make sure the slot exists
+
+				current_parsed_time += consultation_duration
+			end
+
+			last_val = schedules[day].index(rounded_times.last)
+			schedule_ends = hour_parser(schedule.ends_at.strftime('%H:%M'))
+			# If Schedule ends after schedule ends parsed, delete the last slot
+			if schedule_ends > end_schedule_parsed && last_val
+				schedules[day].delete_at(last_val)
+			end
+		end
+
+		return schedules
+	end
 
   def account_schedules(duration)
     schedule = account.configuration['schedule'].except('consultation_duration')
@@ -79,4 +91,5 @@ class AvailableHoursController < ApiController
     end
     result
   end
-end
+
+	end
